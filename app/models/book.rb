1b4e6012001688bd23b33b59e9af19eb9c820c93
Rullian Rong <naillur.gnor@gmail.com>
2022-10-14 15:50:45 +0800

    finalized design and routes, added a logo

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
     validates :title, presence: true
     validates :link, presence: true
-    before_create {|book| book.title = book.title.capitalize}
+    before_create {|book| book.title = book.title.split.map(&:capitalize).join(' ')}
 end
